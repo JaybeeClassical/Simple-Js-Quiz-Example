367943318489b560265bda3a07c021ca83a41ac9
JaybeeGeek <johnboscoobiejesi@gmail.com>
2019-06-10 20:32:05 +0100

    another commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// handles when next button on the index.html is being clicked 
+const next = document.getElementById("next");
+const previous = document.getElementById("prev");
+const submit = document.getElementById("submit");
+
+next.addEventListener('click', function () {
+    document.getElementById('tab1').style.display = 'none';
+    document.getElementById('tab2').style.display = 'block';
+});
+
+// handles event for the previous button 
+previous.addEventListener('click', function () {
+    document.getElementById('tab1').style.display = 'block';
+    document.getElementById('tab2').style.display = 'none';
+})
+
+
+
+
+
